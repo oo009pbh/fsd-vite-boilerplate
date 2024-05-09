97294f9b40bbe8f2219766e90d189aaaca4861b2
ParkByeongHoon <oo009pbh@gmail.com>
2024-05-09 21:00:02 +0900

    [app] EDIT: context 삭제

--- client/service/src/common/context/date/index.tsx
+++
@@ -1,7 +0,0 @@
-import contextFactory from '@context/contextFactory';
-
-import useGlobalDateContextState from './useGlobalDateContextState';
-
-const { Provider, useContext } = contextFactory(useGlobalDateContextState);
-
-export { Provider as DateProvider, useContext as useDateContext };
--- client/service/src/common/context/system/index.tsx
+++
@@ -1,7 +0,0 @@
-import contextFactory from '@context/contextFactory';
-
-import useGlobalSystemContextState from './useGlobalSystemContextState';
-
-const { Provider, useContext } = contextFactory(useGlobalSystemContextState);
-
-export { Provider as SystemProvider, useContext as useSystemContext };
