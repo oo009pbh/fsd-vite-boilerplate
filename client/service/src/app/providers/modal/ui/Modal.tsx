7f7eea698bed5f4100661d72b4cbf3491fdb008d
ParkByeongHoon <oo009pbh@gmail.com>
2024-05-14 23:05:28 +0900

    [app] EDIT: todo 문구 수정

--- client/service/src/app/providers/modal/ui/Modal.tsx
+++ client/service/src/app/providers/modal/ui/Modal.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 // Component
 import CenterModal from '@molecule/CenterModal';
 // todo: fsd 아키텍처와도 충돌하는 부분, 수정 필요
+// 애초에 이런 contextFactory 자체가 fsd와 충돌하는것 같음
 // eslint-disable-next-line import/no-cycle
 import { useModalContext } from '@app/providers';
 
